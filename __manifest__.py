b6dc9e179b04ff74a09e2131a8f087f19625591f
Moaz Elbahr <moazelbahr@gmail.com>
2025-07-06 11:23:32 +0300

    Analytic Account Field Added To Purchase order, Sale order, Vendor bill, Customer invoice, Refund, Credit note

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name' : "account_analytic_field",
+    'description': 'Account Analytic Field',
+    'author' : "Moaz Elbahr",
+    'category': '',
+    'version': '18.0.0.1.0',
+    'depends': ['base', 'purchase', 'accountant', 'sale_management'],
+    'data': [
+        "views/views.xml"
+    ],
+}
---
+++ models/models.py
@@ -0,0 +1,51 @@
+from odoo import models, fields, api
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    analytic_account = fields.Many2one("account.analytic.account")
+
+
+    @api.onchange("analytic_account")
+    def analytic_account_on_change(self):
+        for rec in self:
+            if rec.analytic_account:
+                for line in rec.order_line:
+                    line.analytic_distribution = {
+                        rec.analytic_account.id: 100.0
+                    }
+
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    analytic_account = fields.Many2one("account.analytic.account")
+
+
+    @api.onchange("analytic_account")
+    def analytic_account_on_change(self):
+        for rec in self:
+            if rec.analytic_account:
+                for line in rec.order_line:
+                    line.analytic_distribution = {
+                        rec.analytic_account.id: 100.0
+                    }
+
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    analytic_account = fields.Many2one("account.analytic.account")
+
+
+    @api.onchange("analytic_account")
+    def analytic_account_on_change(self):
+        for rec in self:
+            if rec.analytic_account:
+                for line in rec.line_ids:
+                    line.analytic_distribution = {
+                        rec.analytic_account.id: 100.0
+                    }
